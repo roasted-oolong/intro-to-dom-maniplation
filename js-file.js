5cdb7e73ffc9c5df209dee55507be769b2eb9aa1
Roasted Oolong <65867683+roasted-oolong@users.noreply.github.com>
2024-09-19 20:32:59 +0000

    Add blue h3 text

--- js-file.js
+++ js-file.js
@@ -2,3 +2,9 @@ const para = document.createElement("p");
 para.textContent = "Hey I'm red!";
 para.style.cssText = "color: red";
 document.body.appendChild(para);
+
+
+const headerThree = document.createElement("h3");
+headerThree.textContent = "I'm a blue h3!";
+headerThree.style.cssText = "color: blue";
+document.body.appendChild(headerThree);
